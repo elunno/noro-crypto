2d22eb07af07dc77790c4c9025d0407ea36d5d58
kourosh <kouroshsharifineyestani@gmail.com>
2025-11-29 09:00:38 -0800

    chore(next): allow remote image loading from CoinGecko via next/image config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    reactCompiler: true,
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "coin-images.coingecko.com",
+                pathname: "/coins/images/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
